722909ff5c031d141de4f73ca0a69fb1521c98d9
Kaempy <campa.banj@gmail.com>
2023-07-07 09:57:29 +0100

    feat: testing

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Home | Blue",
   description:
-    "Receive and make payments easily with Blue, you can make your payments seamlessly, request and receive money, all on one platform.",
+    "Receive and make payments easily with Blue, you can make payments seamlessly, request and receive money, all on one platform.",
 };
 
 const Homepage = () => <Home />;
