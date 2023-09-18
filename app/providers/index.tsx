678fcdafb43bca6234061247cc57ef65f7173239
Kaempy <campa.banj@gmail.com>
2023-09-18 11:16:21 +0100

    ci: fixed issue with cicd and application browser error

--- app/providers/index.tsx
+++ app/providers/index.tsx
@@ -1,13 +1,34 @@
 "use client";
-import { StyleProvider } from "@ant-design/cssinjs";
+
+import {
+  StyleProvider,
+  createCache,
+  extractStyle,
+  px2remTransformer,
+} from "@ant-design/cssinjs";
+import type Entity from "@ant-design/cssinjs/es/Cache";
 import theme from "@styles/theme";
 import { ConfigProvider } from "antd";
-import { ReactNode } from "react";
+import { useServerInsertedHTML } from "next/navigation";
+import { ReactNode, useMemo } from "react";
+
+const px2rem = px2remTransformer({
+  rootValue: 16,
+});
 
 const Providers = ({ children }: { children: ReactNode }) => {
+  const cache = useMemo<Entity>(() => createCache(), []);
+  useServerInsertedHTML(() => (
+    <style
+      id="antd"
+      dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }}
+    />
+  ));
   return (
     <ConfigProvider theme={theme}>
-      <StyleProvider hashPriority="high">{children}</StyleProvider>
+      <StyleProvider hashPriority="high" transformers={[px2rem]} cache={cache}>
+        {children}
+      </StyleProvider>
     </ConfigProvider>
   );
 };
