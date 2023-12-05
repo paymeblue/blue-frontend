ce50553bd8a9f8165ee6329be33a8864bf75693f
Victor Whyte <victordavidwhyte@gmail.com>
2023-12-05 17:21:45 +0100

    chore: correct typo

--- app/(receive_money)/components/select-bank.tsx
+++ app/(receive_money)/components/select-bank.tsx
@@ -129,7 +129,7 @@ const SelectBank = ({
               >
                 {loadingWithdraw
                   ? isWidthrawing
-                    ? "Widthdrawing"
+                    ? "Withdrawing"
                     : "Verifying"
                   : "Verify"}
               </Button>
