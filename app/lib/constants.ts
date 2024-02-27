bf36ec370da8c8b06f70ea266a0d66fa35a81457
Victor Whyte <victordavidwhyte@gmail.com>
2024-02-27 10:25:21 +0100

    feat: business kyc pages

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,9 +1,15 @@
 // const USE_SANDBOX = true;
 const USE_SANDBOX = process.env.NODE_ENV === "development";
+
 export const DOJAH_APP_ID = "65687f5182d14d00405ca29c";
+
 export const DOJAH_PUBLIC_KEY = USE_SANDBOX
   ? "test_pk_lRMxOxymUiSXppBSj1Tzsq3Nl"
   : "prod_pk_z3PlcZdMCy7XJqZsqcqYDzPhI";
+
 export const DOJAH_WIDGET_ID = USE_SANDBOX
   ? "65731f462d489d00417a1b64"
   : "657c1f06830cf00040fcac52";
+
+export const DOJAH_WIDGET_ID_BUSINESS_INTERMEDIATE = "65d8b567a264e90040fe7a10";
+export const DOJAH_WIDGET_ID_BUSINESS_PRO = "65d8f120f2409c004005c52d";
