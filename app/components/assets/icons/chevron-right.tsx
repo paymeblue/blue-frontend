08ba898ea147caa206f272e3d67a0d08604ea4ce
Kaempy <campa.banj@gmail.com>
2024-04-23 17:11:26 +0100

    chore: svg fix

--- app/components/assets/icons/chevron-right.tsx
+++ app/components/assets/icons/chevron-right.tsx
@@ -8,20 +8,20 @@ const ChevronRight = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M12 21.2498C17.108 21.2498 21.25 17.1088 21.25 11.9998C21.25 6.89176 17.108 2.74976 12 2.74976C6.892 2.74976 2.75 6.89176 2.75 11.9998C2.75 17.1088 6.892 21.2498 12 21.2498Z"
         stroke="#4341CD"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M10.5576 15.4709L14.0436 11.9999L10.5576 8.52895"
         stroke="#4341CD"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
